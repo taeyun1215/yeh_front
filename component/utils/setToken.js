01fed3b7b70b6f561bdaabeb1669259dcd4625c9
taeyun1215 <taeyun1215@naver.com>
2023-03-14 17:21:15 +0900

    refactor : dockerfile

--- component/utils/setToken.js
+++ component/utils/setToken.js
@@ -12,8 +12,7 @@ const setToken = async (props) => {
 
     const response = await getToken(cookie.refreshToken)
     if(response.data.success) {
-        // 쿠키에 리프레시 토큰 저장 배포 때는 httponly true 바꾸기
-        setCookie('accessToken', response.data.data.access_token, {expires : expires, httpOnly : true} )
+        setCookie('accessToken', response.data.data.access_token, {expires : expires, httpOnly : false} )
         return new Promise((resolve, reject) => {
             resolve('userLogin')
         })

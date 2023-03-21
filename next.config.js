b05519ff976a00025155cece0cc5f1c6daad7f24
Lee Tae Yun <65766105+taeyun1215@users.noreply.github.com>
2023-03-21 14:42:55 +0900

    test

--- next.config.js
+++ next.config.js
@@ -3,13 +3,16 @@ const path = require('path')
 const nextConfig = {
   reactStrictMode: false,
   output: 'export',
+
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+
   images: {
     domain : ['yeh-bucket.s3.ap-northeast-2.amazonaws.com'],
     unoptimized : true
   },
+
   async redirects() {
     return [
       {
@@ -18,7 +21,17 @@ const nextConfig = {
         permanent: false
       }
     ]
-  }
+  },
+
+  exportPathMap: async function () {
+    return {
+      '/': { page: '/' },
+      '/user': { page: '/user' },
+      '/post': { page: '/post' },
+      '/post/[id]': { page: '/post/[id]' },
+    };
+  },
+
 }
 
 module.exports = nextConfig
